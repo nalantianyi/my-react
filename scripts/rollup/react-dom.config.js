73d0f4852c4bec990921144b1c91599801268527
nalantianyi <103520146@qq.com>
2024-05-23 15:02:17 +0800

    feat: 9.1/9.2 第三种调试方式和测试用例的接入

--- scripts/rollup/react-dom.config.js
+++ scripts/rollup/react-dom.config.js
@@ -9,18 +9,18 @@ const pkgPath = resolvePkgPath(name);
 const pkgDistPath = resolvePkgPath(name, true);
 
 export default [
-	//react-dom
+	// react-dom
 	{
 		input: `${pkgPath}/${module}`,
 		output: [
 			{
 				file: `${pkgDistPath}/index.js`,
-				name: 'index.js',
+				name: 'ReactDOM',
 				format: 'umd'
 			},
 			{
 				file: `${pkgDistPath}/client.js`,
-				name: 'client.js',
+				name: 'client',
 				format: 'umd'
 			}
 		],
@@ -48,5 +48,18 @@ export default [
 				}
 			})
 		]
+	},
+	// react-test-utils
+	{
+		input: `${pkgPath}/test-utils.ts`,
+		output: [
+			{
+				file: `${pkgDistPath}/test-utils.js`,
+				name: 'testUtils',
+				format: 'umd'
+			}
+		],
+		external: ['react-dom', 'react'],
+		plugins: [...getBaseRollupPlugins()]
 	}
 ];
